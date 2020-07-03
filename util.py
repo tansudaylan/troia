5aefc3d6e3508ee76a73f28c0d457a3181c19154
Kartik A Pingle <56364766+HPoincare@users.noreply.github.com>
2020-07-03 18:48:54 -0400

    Update util.py
    Improved plots

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from astropy import constants
+#from astropy import constants
 
 '''
 Equations taken from:
@@ -28,7 +28,8 @@ def mag_max(M, m_star, a):
     return mu_max
 
 M_vals = np.linspace(1.0, 100000.0, 100000)
-mstar_vals = np.linspace(1.0, 100000.0, 100000)
+mstar_vals = np.logspace(1.0, 2.0, 100000)
+print(mstar_vals)
 orbradius_vals = np.linspace(1.0, 100000.0, 100000)
 
 mumax_vals = np.zeros(100000)
@@ -46,7 +47,7 @@ def poleski_main(t, t_0, u_0, t_E):
     return Aofu
 
 t_01 = 20.0
-u_01 = 9.0
+u_01 = 3.325
 t_E1 = 0.008
 
 t_vals1 = np.linspace(3.0, 37.0, 35)
@@ -61,7 +62,6 @@ def rahvar_main(t, t_0, φ, M, m_star, a):
 
     # equation 17
     t_E = ((2 * a)/c) * (np.sqrt(M/(m_star + M))) * 1/24
-
     # equation 18
     tao = (t - t_0)/t_E
     u_0 = np.sqrt(φ * (a / ((2 * R_S))))
@@ -78,61 +78,73 @@ M = 8.5
 m_star = 0.35
 a = 17
 
-t_vals2 = np.linspace(3.0, 37.0, 35)
 
-amp_vals2 = np.zeros(35)
 
-for element in t_vals2:
-    amp_vals2[int(element - 3)] = (rahvar_main(element, t_02, φ, M, m_star, a))
-plt.figure(1)
-plt.plot(t_vals2, amp_vals2, label="Rahvar")
-plt.xlabel("Time (hours)")
-plt.ylabel("Magnfication")
+additive = 0
 
 for element in t_vals1:
-    amp_vals1[int(element - 3)] = (poleski_main(element, t_01, u_01, t_E1))
+    amp_vals1[additive] = (rahvar_main(element, t_02, φ, M, m_star, a))
+    additive += 1
+
+plt.figure(1)
+plt.plot(t_vals1, amp_vals1, label="Rahvar")
+plt.xlabel("Time [Hours]")
+plt.ylabel("Magnfication")
+
+additive = 0
+
+for element in t_vals1:
+    amp_vals1[additive] = (poleski_main(element, t_01, u_01, t_E1))
+    additive += 1
 
 plt.plot(t_vals1, amp_vals1, label="Poleski")
-plt.xlabel("Time")
+plt.xlabel("Time [Hours]")
 plt.ylabel("Magnification")
 plt.legend()
 
+# second set
 
-
-
+additive = 0
 
 for element in M_vals:
-    mumax_vals[int(element - 1)] = (mag_max(element, 1, 1))
+    mumax_vals[additive] = (mag_max(element, 1, 1))
+    additive += 1
 
 plt.figure(2)
 plt.plot(M_vals, mumax_vals)
-plt.xlabel("Mass of Lens")
+plt.xlabel("Mass of Lens [$M_{\odot}$]")
 plt.ylabel("Maximum Magnification")
-plt.title("Variable Mass of Lens, m_star = 1, a = 1")
+plt.title("m_star = 1 [$M_{\odot}$], a = 1 [AU]")
 plt.legend()
 
 mumax_vals = np.zeros(100000)
 
+additive = 0
+
 for element in mstar_vals:
-    mumax_vals[int(element - 1)] = (mag_max(1, element, 1))
+    mumax_vals[additive] = (mag_max(1, element, 1))
+    additive += 1
 
 plt.figure(3)
 plt.plot(mstar_vals, mumax_vals)
-plt.xlabel("Mass of Source")
+plt.xlabel("Mass of Source [$M_{\odot}$]")
 plt.ylabel("Maximum Magnification")
-plt.title("M = 1, Variable m_star, a = 1")
+plt.title("M = 1 [$M_{\odot}$], a = 1 [AU]")
 plt.legend()
 
 mumax_vals = np.zeros(100000)
 
+additive = 0
+
 for element in orbradius_vals:
-    mumax_vals[int(element - 1)] = (mag_max(1, 1, element))
+    mumax_vals[additive] = (mag_max(1, 1, element))
+    additive += 1
 
 plt.figure(4)
 plt.plot(orbradius_vals, mumax_vals)
-plt.xlabel("Orbital Radius")
+plt.xlabel("Orbital Radius [AU]")
 plt.ylabel("Maximum Magnification")
-plt.title("M = 1, m_star = 1, Variable Orbital Radius")
+plt.title("M = 1 [$M_{\odot}$], m_star = 1 [$M_{\odot}$]")
 plt.legend()
 
 plt.show()
